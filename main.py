08d75e0773a3f1aba22e243262a3a07fcad7521e
MANISHITA DEY <deymani2752.9119@gmail.com>
2021-12-07 20:05:50 +0530

    Change SQLite to Postgres

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
 # CONNECT TO DATABASE
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL1', "sqlite:///cafes.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Bootstrap(app)
